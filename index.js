dc8c95b364df3280546e352f9b94b982f33f670e
Shubham Game <shubhamg.tri@gmail.com>
2023-02-07 17:28:59 +0530

    - Updated Index

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 function setCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-      var date = new Date();
-      date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-      expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toUTCString();
   }
-  export function getProspUsername(name = "prosp_username") {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(";");
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == " ") c = c.substring(1, c.length);
-      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+export function getProspUsername(name = "prosp_username") {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
   }
+  return null;
+}
 
-export const logProsperety = window.logProsperety
+export const logProsperety = window.logProsperety;
 export default function configureProsperety() {
-window.prosp_data = window.prosp_data || [];
+  function prosp_tag(key, val) {
+    var obj = Object.create(null);
+    obj[key] = val;
+    prosp_data.push(obj);
+  }
+  prosp_tag("js_time", Date.now());
+  prosp_tag("config", "PROSP-9ul81zLpuFGn9nV");
+
+  var script = document.createElement("script");
+
+  script.src = "http://127.0.0.1:8000/js/scripts/analytics.js";
+
+  document.head.appendChild(script);
+
+  window.prosp_data = window.prosp_data || [];
   function prosp_tag(key, val) {
     var obj = Object.create(null);
     obj[key] = val;
@@ -29,7 +43,6 @@ window.prosp_data = window.prosp_data || [];
   prosp_tag("js_time", Date.now());
   prosp_tag("config", "PROSP-9ul81zLpuFGn9nV");
   // some cookie functions
- 
 
   //get params
   const url_win_string = window.location.href;
