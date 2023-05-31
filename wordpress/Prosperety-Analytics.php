7d37cc09327af8518864b998025a1fe26b9b3a23
Karunesh vadaliya <karunesh.vadaliya@gmail.com>
2023-05-31 21:59:08 +0530

    Readme and jquery file added

--- wordpress/Prosperety-Analytics.php
+++ wordpress/Prosperety-Analytics.php
@@ -6,17 +6,41 @@ Description: Prosperety Analytics Tool.
 Author: Naresh Gupta
 */
 
-
 /**
  * Function Prosperety Analytics Store User
  * When URL hit with username querystring, function will store username in local storage with name "pAnalyticsUserName"
  * And then whenever user click on "a", "li", "button" press, The function will call webhook to presperety.
  */
 function pAnalyticsStoreUser() {
-    $output = '';
+    $output = '<script>
+            function setCookie(name, value, days) {
+                var expires = "";
+                if (days) {
+                    var date = new Date();
+                    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+                    expires = "; expires=" + date.toUTCString();
+                }
+                document.cookie = name + "=" + (value || "") + expires + "; path=/";
+            }
+            function getProspUsername(name = "pAnalyticsUserName") {
+                var nameEQ = name + "=";
+                var ca = document.cookie.split(";");
+                for (var i = 0; i < ca.length; i++) {
+                    var c = ca[i];
+                    while (c.charAt(0) == " ") c = c.substring(1, c.length);
+                    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+                }
+                return null;
+            }
+        </script>';
     if (isset($_GET['username'])) {
-        $output = "<script>
-            localStorage.setItem('pAnalyticsUserName', '". $_GET['username']."');
+        $cookie_name = 'pAnalyticsUserName';
+        $cookie_value = $_GET['username'];
+        $days = 1;
+
+        $output .= "<script>
+                console.log('user name found');
+                setCookie('$cookie_name', '$cookie_value', '$days');
             </script>";
     }
     $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://';
@@ -24,9 +48,12 @@ function pAnalyticsStoreUser() {
     $currentURL = $protocol . $host . $_SERVER['REQUEST_URI'];
 
     $output .= "
-    <script src='https://code.jquery.com/jquery-3.6.0.min.js'></script>
+    <script src='jquery-3.6.0.min.js'></script>
     <script src='https://prosperety.tritest.link/js/scripts/analytics.js'></script>
     <script>
+    const pAnalyticsToURL = 'http://127.0.0.1:8000/analytics/webhook';
+    const requestFrom = '". $currentURL ."';
+
     window.prosp_data = window.prosp_data || [];
     function prosp_tag(key, val) {
         var obj = Object.create(null);
@@ -36,8 +63,6 @@ function pAnalyticsStoreUser() {
     prosp_tag('js_time', Date.now());
     prosp_tag('config', 'PROSP-9ul81zLpuFGn9nV');
 
-    const pAnalyticsToURL = 'http://127.0.0.1:8000/analytics/webhook';
-    const requestFrom = '". $currentURL ."';
     jQuery( document ).ready(function($) {
         $('a').click(function() {
             pAnalyticsWebHook('a', $(this).text());
@@ -49,7 +74,7 @@ function pAnalyticsStoreUser() {
             pAnalyticsWebHook('li', $(this).text());
         });
     });
-    const pAnalyticsUserName = localStorage.getItem('pAnalyticsUserName');
+    const pAnalyticsUserName = getProspUsername('pAnalyticsUserName');
     function pAnalyticsWebHook(elementType, elementText){
         if(pAnalyticsUserName) {
             logProsperety({
@@ -77,3 +102,4 @@ function pAnalyticsStoreUser() {
  * Second argument is call above function.
  */
 add_shortcode( 'pAnalyticsInitialize', 'pAnalyticsStoreUser' );
+?>
